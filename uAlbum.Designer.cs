8784b4a41a2775fb700ef57574d160eeb6a77e70
Tính Nguyễn Trung <21520115@ms.uit.edu.vn>
2023-02-03 02:59:15 +0700

    updateabout1

--- uAlbum.Designer.cs
+++ uAlbum.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // uAlbum
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "uAlbum";
+            this.Size = new System.Drawing.Size(1106, 537);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
