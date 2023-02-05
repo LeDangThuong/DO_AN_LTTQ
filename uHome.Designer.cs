e43592cb4e1bd3d185e2ffe76a43fe97320e6401
Thương Lê Đăng <21522654@ms.uit.edu.vn>
2023-02-05 12:00:30 +0700

    Home

--- uHome.Designer.cs
+++ uHome.Designer.cs
@@ -29,31 +29,71 @@
         private void InitializeComponent()
         {
             this.flowPanelMedia = new System.Windows.Forms.FlowLayoutPanel();
-            this.metroSetLabel1 = new MetroSet_UI.Controls.MetroSetLabel();
+            this.metroSetTabControl1 = new MetroSet_UI.Controls.MetroSetTabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
             this.flowPanelMedia.SuspendLayout();
+            this.metroSetTabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // flowPanelMedia
             // 
-            this.flowPanelMedia.Controls.Add(this.metroSetLabel1);
+            this.flowPanelMedia.Controls.Add(this.metroSetTabControl1);
             this.flowPanelMedia.Location = new System.Drawing.Point(3, 3);
             this.flowPanelMedia.Name = "flowPanelMedia";
             this.flowPanelMedia.Size = new System.Drawing.Size(418, 240);
             this.flowPanelMedia.TabIndex = 0;
             // 
-            // metroSetLabel1
+            // metroSetTabControl1
             // 
-            this.metroSetLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.metroSetLabel1.IsDerivedStyle = true;
-            this.metroSetLabel1.Location = new System.Drawing.Point(3, 0);
-            this.metroSetLabel1.Name = "metroSetLabel1";
-            this.metroSetLabel1.Size = new System.Drawing.Size(100, 23);
-            this.metroSetLabel1.Style = MetroSet_UI.Enums.Style.Light;
-            this.metroSetLabel1.StyleManager = null;
-            this.metroSetLabel1.TabIndex = 1;
-            this.metroSetLabel1.Text = "Home";
-            this.metroSetLabel1.ThemeAuthor = "Narwin";
-            this.metroSetLabel1.ThemeName = "MetroLite";
+            this.metroSetTabControl1.AnimateEasingType = MetroSet_UI.Enums.EasingType.CubeOut;
+            this.metroSetTabControl1.AnimateTime = 200;
+            this.metroSetTabControl1.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.metroSetTabControl1.Controls.Add(this.tabPage1);
+            this.metroSetTabControl1.Controls.Add(this.tabPage2);
+            this.metroSetTabControl1.Controls.Add(this.tabPage3);
+            this.metroSetTabControl1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.metroSetTabControl1.IsDerivedStyle = true;
+            this.metroSetTabControl1.ItemSize = new System.Drawing.Size(100, 38);
+            this.metroSetTabControl1.Location = new System.Drawing.Point(3, 3);
+            this.metroSetTabControl1.Name = "metroSetTabControl1";
+            this.metroSetTabControl1.SelectedIndex = 0;
+            this.metroSetTabControl1.SelectedTextColor = System.Drawing.Color.White;
+            this.metroSetTabControl1.Size = new System.Drawing.Size(403, 226);
+            this.metroSetTabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.metroSetTabControl1.Speed = 100;
+            this.metroSetTabControl1.Style = MetroSet_UI.Enums.Style.Light;
+            this.metroSetTabControl1.StyleManager = null;
+            this.metroSetTabControl1.TabIndex = 0;
+            this.metroSetTabControl1.ThemeAuthor = "Narwin";
+            this.metroSetTabControl1.ThemeName = "MetroLite";
+            this.metroSetTabControl1.UnselectedTextColor = System.Drawing.Color.Gray;
+            this.metroSetTabControl1.UseAnimation = false;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 42);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(395, 180);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 42);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Size = new System.Drawing.Size(395, 180);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 42);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(395, 180);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
             // 
             // uHome
             // 
@@ -61,8 +101,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.flowPanelMedia);
             this.Name = "uHome";
-            this.Size = new System.Drawing.Size(451, 270);
+            this.Size = new System.Drawing.Size(576, 324);
             this.flowPanelMedia.ResumeLayout(false);
+            this.metroSetTabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -70,6 +111,9 @@
         #endregion
 
         public System.Windows.Forms.FlowLayoutPanel flowPanelMedia;
-        private MetroSet_UI.Controls.MetroSetLabel metroSetLabel1;
+        private MetroSet_UI.Controls.MetroSetTabControl metroSetTabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
     }
 }
